f9d1b6098f49bc6f003b9f94693275ba7277901e
nonnodacciaio <nonnodacciaio@gmail.com>
2022-06-09 12:32:55 +0200

    Altre Cose

---
+++ incrediblestyle/src/main/java/com/generationitaly/incrediblestyle/auth/UtenteRepository.java
@@ -0,0 +1,9 @@
+package com.generationitaly.incrediblestyle.auth;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtenteRepository extends CrudRepository<Utente, Integer>, UserDAO {
+
+}
---
+++ incrediblestyle/src/main/java/com/generationitaly/incrediblestyle/security/Roles.java
@@ -0,0 +1,6 @@
+package com.generationitaly.incrediblestyle.security;
+
+public interface Roles {
+    static String ADMIN = "ADMIN";
+    static String USER = "USER";
+}
